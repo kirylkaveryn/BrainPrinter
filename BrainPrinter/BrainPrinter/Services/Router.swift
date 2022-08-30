ba724b9a6347796d9f0de7c7ece9dada2962ef12
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2022-08-31 00:31:27 +0300

    feat: add ability to convert PDFDocument to Array of Images

--- BrainPrinter/BrainPrinter/Services/Router.swift
+++ BrainPrinter/BrainPrinter/Services/Router.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 import PhotosUI
 import VisionKit
+import PDFKit
 
 protocol RouterProtocol {
     init(navigationController: UINavigationController)
@@ -37,16 +38,16 @@ class Router: NSObject, RouterProtocol {
             navigationController.present(phtotosPickerViewController, animated: true)
 
         case .document:
-            let documentBrowserViewController = UIDocumentBrowserViewController()
-            documentBrowserViewController.allowsDocumentCreation = false
-            documentBrowserViewController.allowsPickingMultipleItems = false
-            documentBrowserViewController.delegate = self
-            navigationController.present(documentBrowserViewController, animated: true)
-            
-//            let documentPickerViewController = UIDocumentPickerViewController()
-//            documentPickerViewController.delegate = self
-//            documentPickerViewController.allowsMultipleSelection = false
-//            navigationController.present(documentPickerViewController, animated: true)
+//            let documentBrowserViewController = UIDocumentBrowserViewController()
+//            documentBrowserViewController.allowsDocumentCreation = false
+//            documentBrowserViewController.allowsPickingMultipleItems = false
+//            documentBrowserViewController.delegate = self
+//            navigationController.present(documentBrowserViewController, animated: true)
+//
+            let documentPickerViewController = UIDocumentPickerViewController(forOpeningContentTypes: [.image, .text, .html, .pdf, .png, .jpeg, .bmp,], asCopy: true)
+            documentPickerViewController.delegate = self
+            documentPickerViewController.allowsMultipleSelection = false
+            navigationController.present(documentPickerViewController, animated: true)
 
             
         case .scan:
@@ -87,7 +88,7 @@ extension Router: UIImagePickerControllerDelegate, UINavigationControllerDelegat
         if let completion = completion {
             completion(resultResource)
         }
-        self.cleanUpResultResource()
+        cleanUpResultResource()
     }
 }
 
@@ -144,14 +145,72 @@ extension Router: VNDocumentCameraViewControllerDelegate {
     }
 }
 
-extension Router: UIDocumentBrowserViewControllerDelegate {
-    
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
-        print(documentURLs)
+//extension Router: UIDocumentBrowserViewControllerDelegate {
+//
+//    func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
+//        guard let pickedURL = documentURLs.first, let urlComponents = URLComponents(url: pickedURL, resolvingAgainstBaseURL: true) else {
+//            navigationController.dismiss(animated: true)
+//            return
+//        }
+//
+//
+//        navigationController.dismiss(animated: true)
+//    }
+//
+//    func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
+//        print(destinationURL)
+//    }
+//}
+
+// MARK: - UIDocumentPickerDelegate
+
+extension Router: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let pickedURL = urls.first else {
+            navigationController.dismiss(animated: true)
+            return
+        }
+        
+        guard let completion = completion else {
+            navigationController.dismiss(animated: true)
+            return
+        }
+        // check if image imported
+        if let image = UIImage(contentsOfFile: pickedURL.path) {
+            completion([image])
+        }
+        
+        // check if PDF imported
+        let images = getImagesFromPDF(url: pickedURL)
+        if images.count > 0 {
+            completion(images)
+        }
+        
         navigationController.dismiss(animated: true)
     }
     
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
-        print(destinationURL)
+    /// Converts and returns PDFDocument from profvided URL to Array of UIImage
+    private func getImagesFromPDF(url: URL) -> [UIImage] {
+        var images: [UIImage] = []
+        if let document = CGPDFDocument(url as CFURL) {
+            for index in 1..<document.numberOfPages + 1 {
+                guard let page = document.page(at: index) else { return images }
+                let pageRect = page.getBoxRect(.mediaBox)
+                let renderer = UIGraphicsImageRenderer(size: pageRect.size)
+                let image = renderer.image { ctx in
+                    UIColor.white.set()
+                    ctx.fill(pageRect)
+                    
+                    ctx.cgContext.translateBy(x: 0.0, y: pageRect.size.height)
+                    ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+                    
+                    ctx.cgContext.drawPDFPage(page)
+                }
+                images.append(image)
+            }
+        }
+        return images
     }
 }
+
+
